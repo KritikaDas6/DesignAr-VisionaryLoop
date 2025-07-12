fb23c62cf65a76f92a7559118c7e6b10fdf505a4
kritika das <daskritika.2625@gmail.com>
2025-07-12 11:52:06 -0700

    Added the ai trace script

--- Assets/GenAI/InteractableImageGenerator.ts
+++ Assets/GenAI/InteractableImageGenerator.ts
@@ -13,22 +13,22 @@ export class InteractableImageGenerator extends BaseScriptComponent {
     ])
   )
   private modelProvider: string = "OpenAI";
+
   @ui.separator
-  @input
-  private image: Image;
-  @input
-  private textDisplay: Text;
-  @input
-  private asrQueryController: ASRQueryController;
-  @input
-  private spinner: SceneObject;
+  @input private image: Image;
+  @input private textDisplay: Text;
+  @input private asrQueryController: ASRQueryController;
+  @input private spinner: SceneObject;
+
   private imageGenerator: ImageGenerator = null;
 
   onAwake() {
     this.imageGenerator = new ImageGenerator(this.modelProvider);
+
     let imgMat = this.image.mainMaterial.clone();
     this.image.clearMaterials();
     this.image.mainMaterial = imgMat;
+
     this.createEvent("OnStartEvent").bind(() => {
       this.spinner.enabled = false;
       this.asrQueryController.onQueryEvent.add((query) => {
@@ -38,20 +38,23 @@ export class InteractableImageGenerator extends BaseScriptComponent {
   }
 
   createImage(prompt: string) {
+    const finalPrompt = "Tracing line with Transparent Background of " + prompt;
+
     this.spinner.enabled = true;
-    this.textDisplay.text = "Generating: " + prompt;
+    this.textDisplay.text = "Generating: " + finalPrompt;
+
     this.imageGenerator
-      .generateImage(prompt)
+      .generateImage(finalPrompt)
       .then((image) => {
         print("Image generated successfully: " + image);
         this.textDisplay.text = prompt;
         this.image.mainMaterial.mainPass.baseTex = image;
-        this.textDisplay.text = prompt;
         this.spinner.enabled = false;
       })
       .catch((error) => {
+        print(finalPrompt);
         print("Error generating image: " + error);
-        this.textDisplay.text = "Error Generating Image";
+        this.textDisplay.text = "Error Generating Image " + finalPrompt;
         this.spinner.enabled = false;
       });
   }
